07d86140f00fd77921b00581bb4f3f6c5f75578c
nuhptr <nugrahaadi733@gmail.com>
2021-06-05 09:27:03 +0700

    aritmatika

---
+++ php_dasar_pzn/Aritmatika/LogicOperator.php
@@ -0,0 +1,17 @@
+<?php
+
+// TODO : oprerator( && / and, || / or,
+// TODO : ! (true jika value false), Xor (salah satu true tapi tidak keduanya)
+
+var_dump(true && true); // TODO : true
+var_dump(true && false); // TODO : false
+
+var_dump(true || true); // TODO : true
+var_dump(true || false); // TODO : true
+
+var_dump(true xor true); // TODO : false
+var_dump(true xor false); // TODO : true
+
+var_dump(!true); // TODO : false
+var_dump(!false); // TODO : true
+
