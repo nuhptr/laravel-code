a39926d60484133477d290a6264549db97db7dbc
nuhptr <nugrahaadi733@gmail.com>
2021-06-05 23:13:07 +0700

    const and how to use it in another class

---
+++ php_oop_pzn/Constant.php
@@ -0,0 +1,8 @@
+<?php
+require_once "data\Person.php";
+
+const APP_VERSION = "1.0.0";
+
+// TODO : cara memanggil constant dari class lain
+echo Person::AUTHOR .PHP_EOL; // Adi Nugraha Putra
+echo APP_VERSION. PHP_EOL; // 1.0.0
