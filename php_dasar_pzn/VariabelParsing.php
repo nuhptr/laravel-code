cfd2af0e6e5100aa32795177d556b77fa1f03a20
nuhptr <nugrahaadi733@gmail.com>
2021-06-05 13:25:31 +0700

    echo "$name"

---
+++ php_dasar_pzn/VariabelParsing.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Adi Nugraha Putra";
+
+echo "Name : $name" . PHP_EOL; // Name  : Adi Nugraha Putra
+
+// TODO : Curly Brace
+$var = "var";
+echo "This is {$var}s" .PHP_EOL; // this is vars
