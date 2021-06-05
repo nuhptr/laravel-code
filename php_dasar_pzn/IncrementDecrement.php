8f48a88278a60ca0e564712b69531de5d5c9926c
nuhptr <nugrahaadi733@gmail.com>
2021-06-05 09:33:52 +0700

    update susunan

---
+++ php_dasar_pzn/IncrementDecrement.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * TODO : $a++ (Kembalikan $a lalu naikan 1 angka)
+ * TODO : ++$a (naikan satu angka lalu kembalikan $a)
+ * TODO : $a-- (kembalikan $a lalu turunkan 1 angka)
+ * TODO : --$a (turunkan 1 angka lalu kembalikan $a)
+ */
+
+$a = 10;
+$b = ++$a;
+
+var_dump($b);
+var_dump($a);
