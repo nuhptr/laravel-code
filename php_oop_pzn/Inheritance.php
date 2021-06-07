914f1c7cd68c1540e144e3df6ac602086c000d3a
nuhptr <nugrahaadi733@gmail.com>
2021-06-07 12:43:17 +0700

    inheritance

--- php_oop_pzn/Inheritance.php
+++ php_oop_pzn/Inheritance.php
@@ -1,6 +1,23 @@
 <?php
 
-require_once "data\Manager.php";
+class Manager
+{
+    var ?string $name = null;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    function sayHello(?string $you)
+    {
+        if (is_null($this->name)) {
+            return "hi, $this->name" .PHP_EOL;
+        } else {
+            echo "Hai $you from $this->name" . PHP_EOL;
+        }
+    }
+}
 
 // TODO : Inheritance dengan class Manager
 class VicePresident extends Manager {
