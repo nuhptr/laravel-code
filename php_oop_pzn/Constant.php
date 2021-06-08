275a1b7321b91f0ad1a084fcb41aab4ffc694f4d
nuhptr <nugrahaadi733@gmail.com>
2021-06-08 16:56:52 +0700

    Constant

--- php_oop_pzn/Constant.php
+++ php_oop_pzn/Constant.php
@@ -1,7 +1,7 @@
 <?php
 require_once "data\Person.php";
 
-const APP_VERSION = "1.0.0";
+const APP_VERSION = "1.0.0"; // tidak bisa diubah lagi valuenya
 
 // TODO : cara memanggil constant dari class lain
 echo Person::AUTHOR .PHP_EOL; // Adi Nugraha Putra
